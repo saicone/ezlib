036efc3d64ce38c809f91f886334d8275f2d3e42
Rubén <44579213+Rubenicos@users.noreply.github.com>
2026-01-10 10:29:16 -0600

    build: update gradle

--- src/main/java/com/saicone/ezlib/Ezlib.java
+++ src/main/java/com/saicone/ezlib/Ezlib.java
@@ -33,7 +33,7 @@ public class Ezlib {
     /**
      * Current ezlib version to download ezlib loader.
      */
-    public static String VERSION = "${version}";
+    public static String VERSION = "project.version";
 
     /**
      * Change current ezlib version to another one, use "-SNAPSHOT" for latest commit.
